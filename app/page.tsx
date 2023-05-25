6a1f5edc87b4d6129a645f799e7edce6000eaafe
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-05-25 18:03:09 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
+    <main className="bg-gray-900 text-white">
       <h3 className="text-red-400 p-1 font-extrabold m-4">Hello Next 13</h3>
       <p className="m-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
