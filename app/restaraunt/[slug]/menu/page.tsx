ca0fb297c64cad792066ff83b975a4a5008b8db2
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-05-26 18:34:46 +0200

    fixes

--- app/restaraunt/[slug]/menu/page.tsx
+++ app/restaraunt/[slug]/menu/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Link from "next/link";
 
 export default function RestarauntMenu() {
   return (
@@ -6,10 +6,10 @@ export default function RestarauntMenu() {
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
-          <a href="" className="font-bold text-gray-700 text-2xl">
+          <Link href="" className="font-bold text-gray-700 text-2xl">
             {" "}
             OpenTable{" "}
-          </a>
+          </Link>
           <div>
             <div className="flex">
               <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
@@ -32,14 +32,14 @@ export default function RestarauntMenu() {
           <div className="bg-white w-[100%] rounded p-3 shadow">
             {/* RESAURANT NAVBAR */}
             <nav className="flex text-reg border-b pb-2">
-              <a href="" className="mr-7">
+              <Link href="" className="mr-7">
                 {" "}
                 Overview{" "}
-              </a>
-              <a href="" className="mr-7">
+              </Link>
+              <Link href="" className="mr-7">
                 {" "}
                 Menu{" "}
-              </a>
+              </Link>
             </nav>
             {/* RESAURANT NAVBAR */} {/* MENU */}
             <main className="bg-white mt-5">
--- app/restaraunt/[slug]/page.tsx
+++ app/restaraunt/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Link from "next/link";
 
 export default function RestarauntDetails() {
   return (
@@ -6,10 +6,10 @@ export default function RestarauntDetails() {
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
-          <a href="" className="font-bold text-gray-700 text-2xl">
+          <Link href="" className="font-bold text-gray-700 text-2xl">
             {" "}
             OpenTable{" "}
-          </a>
+          </Link>
           <div>
             <div className="flex">
               <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
@@ -32,14 +32,14 @@ export default function RestarauntDetails() {
           <div className="bg-white w-[70%] rounded p-3 shadow">
             {/* RESAURANT NAVBAR */}
             <nav className="flex text-reg border-b pb-2">
-              <a href="" className="mr-7">
+              <Link href="" className="mr-7">
                 {" "}
                 Overview{" "}
-              </a>
-              <a href="" className="mr-7">
+              </Link>
+              <Link href="" className="mr-7">
                 {" "}
                 Menu{" "}
-              </a>
+              </Link>
             </nav>
             {/* RESAURANT NAVBAR */} {/* TITLE */}
             <div className="mt-4 border-b pb-6">
@@ -172,7 +172,7 @@ export default function RestarauntDetails() {
         </div>
         {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */}{" "}
         {/* RESERVATION
-    CARD PORTION */}
+      CARD PORTION */}
       </main>
     </main>
   );
