5592a3528e7f82c785c29d300a4ba768df60522c
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-05-30 14:38:29 +0200

    extract komponents

--- app/reservation/[slug]/page.tsx
+++ app/reservation/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/app/comps/Header";
+import Navbar from "@/app/comps/Navbar";
 import Link from "next/link";
 
 export default function ReservationPage() {
@@ -5,45 +7,13 @@ export default function ReservationPage() {
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <Link href="" className="font-bold text-gray-700 text-2xl">
-            {" "}
-            OpenTable{" "}
-          </Link>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                Sign in
-              </button>
-              <button className="border p-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         {/* NAVBAR END */}
         <div className="border-t h-screen">
           <div className="py-9 w-3/5 m-auto">
-            {/* HEADER */}
-            <div>
-              <h3 className="font-bold">You're almost done!</h3>
-              <div className="mt-5 flex">
-                <img
-                  src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-                  alt=""
-                  className="w-32 h-18 rounded"
-                />
-                <div className="ml-4">
-                  <h1 className="text-3xl font-bold">
-                    Aiāna Restaurant Collective
-                  </h1>
-                  <div className="flex mt-3">
-                    <p className="mr-6">Tues, 22, 2023</p>
-                    <p className="mr-6">7:30 PM</p>
-                    <p className="mr-6">3 people</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* HEADER */} {/* FORM */}
+            <Header />
+
+            {/* FORM */}
             <div className="mt-10 flex flex-wrap justify-between w-[660px]">
               <input
                 type="text"
